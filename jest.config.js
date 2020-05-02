d169a8a40e370b607313427d963c5b0677b2bf39
Luis Catano <luis.catano@grability.com>
2020-05-02 14:04:03 -0500

    Husky added

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,19 @@
 module.exports = {
-  testEnvironment: "jsdom",
-  roots: ["<rootDir>/src"],
-  preset: "ts-jest",
-  setupFilesAfterEnv: ["<rootDir>/setupTest.ts"],
-  transform: {
-    "^.+\\.tsx?$": "ts-jest"
-  },
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  testPathIgnorePatterns: ["<rootDir>/build/", "<rootDir>/node_modules/"],
-  snapshotSerializers: ["enzyme-to-json/serializer"],
+    testEnvironment: "jsdom",
+    roots: ["<rootDir>/src"],
+    preset: "ts-jest",
+    setupFilesAfterEnv: ["<rootDir>/setupTest.ts"],
+    transform: {
+        "^.+\\.tsx?$": "ts-jest",
+    },
+    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+    testPathIgnorePatterns: ["<rootDir>/build/", "<rootDir>/node_modules/"],
+    snapshotSerializers: ["enzyme-to-json/serializer"],
 
-  globals: {
-    "ts-jest": {
-      tsConfig: "<rootDir>/tsconfig.jest.json"
-    }
-  }
+    globals: {
+        "ts-jest": {
+            tsConfig: "<rootDir>/tsconfig.jest.json",
+        },
+    },
 };
